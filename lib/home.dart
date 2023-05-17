9bd849115832cc2e4d433b1c934040ed99f4a616
Wesley Marcos <wesley.marcos@gec.inatel.br>
2023-05-16 22:34:43 -0300

    avançando a tela 'Home'

--- lib/home.dart
+++ lib/home.dart
@@ -13,13 +13,54 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/background.png'),
-              fit: BoxFit.cover,
-            ),),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
         ),
+        padding: EdgeInsets.only(top: 45, left: 30, right: 30),
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, top: 0, right: 20),
+          child: ListView(
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    child: Image.asset('images/Logo_teste_semFundo.png'),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Conect+",
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+
+              Text(
+                "Você por dentro \nda sua rede",
+                //style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Entrar"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Cadastrar"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
